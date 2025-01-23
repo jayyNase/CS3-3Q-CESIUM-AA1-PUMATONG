00b12891ba7b54d95ac98038cdb3c9f4d3b6196e
Jay <bjmpumatong22@src.pshs.edu.ph>
2025-01-23 20:31:08 +0800

    Add files via upload

---
+++ script(1).js
@@ -0,0 +1,15 @@
+const pi = 3.14;
+let radius;
+let circumference;
+let surfaceArea;
+let volume;
+
+radius = 50;
+circumference = 2 * pi * radius;
+surfaceArea = 4 * pi * radius * radius * radius;
+volume = (4/3) * pi * radius * radius * radius;
+
+console.log("The radius of the spehere is ", radius);
+console.log("The circumference is ", circumference);
+console.log("The Surface area is ", surfaceArea);
+console.log("The volume is ", volume);
